de32550f1ad9f672b5fcda0a116c2681183ecd07
Nyarlko Yasaka <nyarlko@hotmail.com>
2025-07-15 13:20:21 +0300

    Upload 15*07*2025

---
+++ picotool-build/data_locs.cpp
@@ -0,0 +1,7 @@
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> data_locs = {
+    "./","C:/Users/user/.pico-sdk/build/_deps/picotool/"
+};
---
+++ picotool-build/lib/mbedtls/library/CMakeFiles/mbedcrypto.dir/psa_crypto_storage.c.obj.d
@@ -0,0 +1,60 @@
+C:/Users/user/.pico-sdk/lib/mbedtls/library/common.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/mbedtls/config.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/mbedtls/check_config.h
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.42.34433/include/limits.h
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.42.34433/include/vcruntime.h
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.42.34433/include/sal.h
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.42.34433/include/concurrencysal.h
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.42.34433/include/vadefs.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/assert.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/stddef.h
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.42.34433/include/stdint.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/stdlib.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_malloc.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_search.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_wstdlib.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/string.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_memory.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_memcpy_s.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/errno.h
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.42.34433/include/vcruntime_string.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_wstring.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/psa/crypto.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/psa/crypto_platform.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/mbedtls/config_psa.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/psa/crypto_types.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/psa/crypto_values.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/psa/crypto_sizes.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/psa/crypto_struct.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/mbedtls/cmac.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/mbedtls/cipher.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/mbedtls/platform_util.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/mbedtls/platform_time.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/time.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_wtime.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/mbedtls/gcm.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/psa/crypto_driver_contexts_primitives.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/psa/crypto_driver_common.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/psa/crypto_builtin_primitives.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/mbedtls/md2.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/mbedtls/md4.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/mbedtls/md5.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/mbedtls/ripemd160.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/mbedtls/sha1.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/mbedtls/sha256.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/mbedtls/sha512.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/psa/crypto_driver_contexts_composites.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/psa/crypto_builtin_composites.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/psa/crypto_extra.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/psa/crypto_compat.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/mbedtls/ecp.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/mbedtls/bignum.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/stdio.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_wstdio.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_stdio_config.h
+C:/Users/user/.pico-sdk/lib/mbedtls/library/psa_crypto_storage.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/psa/crypto_se_driver.h
+C:/Users/user/.pico-sdk/lib/mbedtls/library/psa_crypto_its.h
+C:/Users/user/.pico-sdk/lib/mbedtls/include/mbedtls/platform.h
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.42.34433/include/stdarg.h
