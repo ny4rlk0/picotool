a078ab33fba67d15dc1896252679874d9b7dc074
Nyarlko Yasaka <nyarlko@hotmail.com>
2025-07-15 13:17:03 +0300

    Clean re-upload

--- picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","C:/Users/user/.pico-sdk/build/_deps/picotool/"
-};
--- picotool-build/lib/mbedtls/library/CMakeFiles/mbedx509.dir/pkcs11.c.obj.d
+++
@@ -1,8 +0,0 @@
-C:/Users/user/.pico-sdk/lib/mbedtls/include/mbedtls/pkcs11.h
-C:/Users/user/.pico-sdk/lib/mbedtls/include/mbedtls/config.h
-C:/Users/user/.pico-sdk/lib/mbedtls/include/mbedtls/check_config.h
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.42.34433/include/limits.h
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.42.34433/include/vcruntime.h
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.42.34433/include/sal.h
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.42.34433/include/concurrencysal.h
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.42.34433/include/vadefs.h
